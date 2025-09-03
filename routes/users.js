0fbddfacd49f5daf4be479bb3501596a34aa5c50
SunnyDev <cristinavranceanu86@gmail.com>
2025-09-03 11:25:52 +0200

    add try/catch into fils users.js

--- routes/users.js
+++ routes/users.js
@@ -33,12 +33,13 @@ router.post("/signup", async (req, res) => {
 
     const token = jwt.sign({ id: savedUser.id }, JWT_SECRET, { expiresIn: "24h" });
 
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: true, // adapte à ton environnement si besoin
-      sameSite: "none",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+  res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 24 * 60 * 60 * 1000,
+});
+
 
     return res.json({ result: true, data: savedUser });
   } catch (error) {
